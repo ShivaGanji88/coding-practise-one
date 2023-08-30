70ec92570ef623775070b71f1fb3a5c8f179282e
shiva ganji <135611043+ShivaGanji88@users.noreply.github.com>
2023-08-30 11:29:40 +0530

    Add files via upload

---
+++ practise.py
@@ -0,0 +1,8 @@
+#coding practise
+#write a program that adds the digits in a 2 digit number. 
+#e.g if the number input was 35 then the output should be 3 + 5 = 8
+
+two_digit_number = input("Type a TWo digit Number: ")
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+print(int(first_number) + int(second_number))
